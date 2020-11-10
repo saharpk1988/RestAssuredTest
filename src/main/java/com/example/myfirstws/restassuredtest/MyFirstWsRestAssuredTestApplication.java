7d6d741a1e6a8858d4154e30c616a7fa775facda
Sahar Pk <sahar.pk88@gmail.com>
2020-11-10 14:53:16 +0100

    Testing Restful Web Service with RestAssured.

---
+++ src/main/java/com/example/myfirstws/restassuredtest/MyFirstWsRestAssuredTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.myfirstws.restassuredtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFirstWsRestAssuredTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyFirstWsRestAssuredTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/myfirstws/restassuredtest/MyFirstWsRestAssuredTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myfirstws.restassuredtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFirstWsRestAssuredTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
